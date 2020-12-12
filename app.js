e72cde79843596240b60ea8d63fbf9e097ea453a
z0ggy <zoggy76@gmail.com>
2020-12-12 22:16:17 +0000

    click me button now working

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-// create hide function witch will hide selected element
+//function after 'click now' button  form will display
 var hide = function()
 {
     document.querySelector('.banner').style.display = 'none';
+    document.querySelector('.form-container').style.cssText = 'opacity: 1; visibility: visible';
+    document.querySelector('.container').style.background = '#cc683c';
+
 }
 
 
 
-// select element and add event listener with hide function
+// select element 'Click Me' button and call hide function after click on it
 document.querySelector('.banner-btn').addEventListener('click', hide);
+
+//select "X" button 
+document.querySelector('.x-btn').addEventListener('click', () => {
+    document.querySelector('banner').style.display = 'flex';
+    document.querySelector('.form-container').style.cssText = 'opacity: 0; visibility: !visible';
+})
