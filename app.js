32155cbc4a41f02b39f792118f777c330513d027
z0ggy <zoggy76@gmail.com>
2020-12-12 22:32:23 +0000

    X button is working

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ var hide = function()
     document.querySelector('.form-container').style.cssText = 'opacity: 1; visibility: visible';
     document.querySelector('.container').style.background = '#cc683c';
 
-}
+};
 
 
 
 // select element 'Click Me' button and call hide function after click on it
 document.querySelector('.banner-btn').addEventListener('click', hide);
 
-//select "X" button 
+//select "X" button  and use arrow function instead custom function
 document.querySelector('.x-btn').addEventListener('click', () => {
-    document.querySelector('banner').style.display = 'flex';
-    document.querySelector('.form-container').style.cssText = 'opacity: 0; visibility: !visible';
-})
+    document.querySelector('.banner').style.display = 'flex';
+    document.querySelector('.form-container').style.cssText = 'opacity: 0; visibility: hidden';
+    document.querySelector('.container').style.cssText = ' background: linear-gradient(rgba(121, 93, 93, 0.8), rgba(0, 0, 0, .7)), url(images/bg1.jpeg) center no-repeat; background-size: cover';
+});
